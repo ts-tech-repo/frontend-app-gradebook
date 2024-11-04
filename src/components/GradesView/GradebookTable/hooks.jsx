09e889e937094bdfaf46dc5f80395461c267e96d
sabid.a <sabid.a@talentsprint.com>
2024-11-04 15:50:01 +0530

    Added letter grade

--- src/components/GradesView/GradebookTable/hooks.jsx
+++ src/components/GradesView/GradebookTable/hooks.jsx
@@ -50,7 +50,7 @@ export const useGradebookTableData = () => {
     ),
     [Headings.email]: (<Fields.Text value={entry.email} />),
     [Headings.fullName]: (<Fields.Text value={entry.full_name} />),
-    [Headings.totalGrade]: letterGradeExists ? `-` : `${roundGrade(entry.percent * 100)}${getLocalizedPercentSign()}`,
+    [Headings.totalGrade]: `${letterGradeExists ? '-' : roundGrade(entry.percent * 100)}${letterGradeExists ? '' : getLocalizedPercentSign()}`,
     ...entry.section_breakdown.reduce((acc, subsection) => ({
       ...acc,
       [subsection.label]: <GradeButton {...{ entry, subsection }} />,
