4827d7df3581eefbbe7d8128bf123473a5d2c70d
sabid.a <sabid.a@talentsprint.com>
2024-11-04 16:16:49 +0530

    Added letter grade

--- src/components/GradesView/index.jsx
+++ src/components/GradesView/index.jsx
@@ -46,7 +46,8 @@ export const GradesView = ({ updateQueryParams }) => {
 
       <div className="d-flex justify-content-between align-items-center mb-2">
         <ScoreViewInput />
-        <BulkManagementControls />
+        {/* #SA || Removed bulk management controls from gradebook page. i.e., "Download Grades" & "Import Grades" */}
+        {/* <BulkManagementControls /> */}
       </div>
 
       <FilteredUsersLabel />
