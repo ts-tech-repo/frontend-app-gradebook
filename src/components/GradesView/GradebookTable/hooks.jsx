13d863447c8ea6817bb3994ec250eef87498ac00
sabid.a <sabid.a@talentsprint.com>
2024-11-09 23:54:19 +0530

    Added condition to check all have letter_grade

--- src/components/GradesView/GradebookTable/hooks.jsx
+++ src/components/GradesView/GradebookTable/hooks.jsx
@@ -47,7 +47,7 @@ export const useGradebookTableData = () => {
     ),
     [Headings.email]: (<Fields.Text value={entry.email} />),
     [Headings.fullName]: (<Fields.Text value={entry.full_name} />),
-    [Headings.totalGrade]: !allLetterGrade(entry.section_breakdown) ? `-` : `${roundGrade(entry.percent * 100)}${getLocalizedPercentSign()}`,
+    [Headings.totalGrade]: allLetterGrade(entry.section_breakdown) ? `-` : `${roundGrade(entry.percent * 100)}${getLocalizedPercentSign()}`,
     ...entry.section_breakdown.reduce((acc, subsection) => ({
       ...acc,
       [subsection.label]: <GradeButton {...{ entry, subsection }} />,
