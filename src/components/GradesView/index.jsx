9076878dde088b8e96bbd80e34e92b9c2d8bbb62
sabid-ansari <113017359+sabid-ansari@users.noreply.github.com>
2024-11-28 18:37:42 +0530

    Enabled Bulk Management Controls in gradebook page

--- src/components/GradesView/index.jsx
+++ src/components/GradesView/index.jsx
@@ -46,8 +46,7 @@ export const GradesView = ({ updateQueryParams }) => {
 
       <div className="d-flex justify-content-between align-items-center mb-2">
         <ScoreViewInput />
-        {/* #SA || letter_grade changes || Removed bulk management controls from gradebook page. i.e., "Download Grades" & "Import Grades" */}
-        {/* <BulkManagementControls /> */}
+        <BulkManagementControls />
       </div>
 
       <FilteredUsersLabel />
